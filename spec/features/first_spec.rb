a3cdf820bd7fa455d2a38da67fa7566800305815
Chattipong <chattipong1@hotmail.com>
2019-01-25 11:45:46 +0700

    editspec-features

--- spec/features/first_spec.rb
+++ spec/features/first_spec.rb
@@ -1,17 +1,17 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-describe 'first screen', js: true do
-  let(:normal_user) { create_normal_user(email: 'normal@user.com') }
-  let(:super_admin) { create_super_admin(email: 'super@user.com') }
+# describe 'first screen', js: true do
+#   let(:normal_user) { create_normal_user(email: 'normal@user.com') }
+#   let(:super_admin) { create_super_admin(email: 'super@user.com') }
 
-  before do
-    super_admin
-    normal_user
+#   before do
+#     super_admin
+#     normal_user
 
-    sign_in normal_user
-  end
+#     sign_in normal_user
+#   end
 
-  it 'show users list' do
-    expect(page).to have_content /super@user\.com.*superadmin.*normal@user\.com.*normal/
-  end
-end
+#   it 'show users list' do
+#     expect(page).to have_content /super@user\.com.*superadmin.*normal@user\.com.*normal/
+#   end
+# end
